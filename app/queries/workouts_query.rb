3bd74337af91b5d018f5ab032c09b103d67607a9
rioaki777 <rioaki777@gmail.com>
2026-03-15 17:54:53 +0900

    feat(PR4): introduce Query Object pattern
    WeightEntriesQuery and WorkoutsQuery encapsulate data retrieval logic,
    making queries reusable and independently testable.

---
+++ app/queries/workouts_query.rb
@@ -0,0 +1,17 @@
+class WorkoutsQuery
+  def self.call(user:, start_date:, end_date:)
+    new(user:, start_date:, end_date:).call
+  end
+
+  def initialize(user:, start_date:, end_date:)
+    @user = user
+    @start_date = start_date
+    @end_date = end_date
+  end
+
+  def call
+    Workout.where(user: @user)
+           .where(recorded_on: @start_date..@end_date)
+           .order(recorded_on: :asc)
+  end
+end
